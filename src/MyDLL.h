1ff3d16e6efbfdd3e61b0ed06e9a1b81220e88be
Thomas Lang <langthom@forwiss.uni-passau.de>
2024-06-06 10:22:42 +0200

    Added library loading test code

---
+++ src/MyDLL.h
@@ -0,0 +1,7 @@
+
+#ifndef OUR_LIB_H
+#define OUR_LIB_H
+
+extern "C" __declspec(dllexport) int bar(int arg);
+
+#endif // OUR_LIB_H
---
+++ src/ThirdPartyLib.h
@@ -0,0 +1,7 @@
+
+#ifndef THIRD_PARTY_LIB_H
+#define THIRD_PARTY_LIB_H
+
+extern "C" __declspec(dllexport) int foo(int arg);
+
+#endif // THIRD_PARTY_LIB_H
